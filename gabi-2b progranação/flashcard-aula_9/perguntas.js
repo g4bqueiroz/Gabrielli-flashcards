220d6f404a5591deef2e0ab6c2f101f0a0c01772
g4bqueiroz <gabrielli.queiroz.nascimento@escola.pr.gov.br>
2025-07-30 11:52:11 -0300

    Add files via upload

---
+++ gabi-2b progranação/flashcard-aula_9/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento geral',
+    'De onde é a invenção do chuveiro elétrico?',
+    'A invenção do chuveiro elétrico é do Brasil'
+)
+
+criaCartao(
+    'Conhecimento geral',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'O nome é João Goulart'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanta casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigada em inglês?',
+    'Thanks'
+)
